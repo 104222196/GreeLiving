af0bfbf8fadb778b2535b9fa3135c73c6433162f
gnut04 <tunggnut2004@gmail.com>
2023-10-02 22:09:31 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello world!";
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <title>Homepage - GreeLiving</title>
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+    <p>Hello world! but it's a paragraph</p>
+    <script type="text/javascript" src="./js/index.js"></script>
+</body>
+
+</html>
